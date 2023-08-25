e9290a57d0c21acba96f77f0f9ca42d46304a04a
Victor Njoroge <victornjoroge2020@gmail.com>
2023-08-25 11:35:44 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_Array=[0,1]
+    
+    while len(fibonacci_Array) < length :
+     newNum= fibonacci_Array[-1] + fibonacci_Array[-2]
+     fibonacci_Array.append(newNum)
+    print(fibonacci_Array)
+print_fibonacci(9)
